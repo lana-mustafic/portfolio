e3b1e0667fd27391ead6998003de6fbbec411550
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-10-26 16:19:37 +0100

    Removed Experience and Blog sections, cleaned navbar and fixed image handling

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,7 @@
 export const personalData = {
   name: "Lana",
-  profile: "", // leave empty until you add your photo later
+  profile: null,
+ // leave empty until you add your photo later
   designation: "Junior Full-Stack Developer",
   description:
     "I’m a soon-to-be Software Engineering graduate specializing in full-stack development with .NET and Angular. I’m passionate about building modern, maintainable applications using clean architecture, secure authentication, and efficient deployment workflows. Currently, I’m expanding my skills in .NET 8, Angular 20, and Docker, and preparing to start my Master’s degree in Software Engineering. I’m always eager to learn new technologies, explore creative ideas, and continuously grow as a developer.",
