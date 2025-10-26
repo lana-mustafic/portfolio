8beb59358305193e9c8dcf8a4dc0469dab721a15
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-10-26 17:50:51 +0100

    feat: Add personal profile picture and complete branding

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,24 +12,24 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
+            © 2025 Portfolio by <span className="text-[#16f2b3]">Lana Mustafić</span>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/lana-mustafic"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
-              <IoStar />
-              <span>Star</span>
+             
+              <span>GitHub</span>
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://linkedin.com/in/lana-mustafic"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
-              <CgGitFork />
-              <span>Fork</span>
+              
+              <span>LinkedIn</span>
             </Link>
           </div>
         </div>
