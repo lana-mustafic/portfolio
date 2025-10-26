81372fb1f99b1e23d7323f0b325a7e784d520a57
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-10-26 17:02:02 +0100

    feat: Personalize portfolio with name Lana Mustafić and updated hero layout

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -24,7 +24,7 @@ function HeroSection() {
             Hello, <br />
             This is {' '}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a `}
+            {` , I'm a`} <br />
             <span className=" text-[#16f2b3]">{personalData.designation}</span>
             .
           </h1>
