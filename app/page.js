081b168c3b336c9640b4e22e38c707e2b6df6527
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-10-27 00:25:56 +0100

    fix: Escape apostrophe to resolve ESLint error

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="min-h-screen flex items-center justify-center bg-[#0d1224] text-white px-4">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Hello, I'm <span className="text-pink-500">Lana Mustafić</span>
+            Hello, I&apos;m <span className="text-pink-500">Lana Mustafić</span>
           </h1>
           <p className="text-xl md:text-2xl text-[#16f2b3] mb-6">
             Junior Full-Stack Developer
