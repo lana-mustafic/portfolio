722dad65a5cd617cb9883488322def89efeae36a
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-10-26 15:54:35 +0100

    Customize portfolio with personal info, skills, projects, and contacts

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
-}
+  email: 'lana-mustafic@outlook.com',
+  phone: '+387 60 335 2668',
+  address: 'Sarajevo, Bosnia and Herzegovina',
+  github: 'https://github.com/lana-mustafic',
+  facebook: '',
+  linkedIn: 'https://linkedin.com/in/lana-mustafic',
+  twitter: '',
+  stackOverflow: '',
+  devUsername: '',
+};
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,18 @@
 export const personalData = {
-  name: "ABU SAID",
-  profile: '/profile.png',
-  designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  name: "Lana",
+  profile: "", // leave empty until you add your photo later
+  designation: "Junior Full-Stack Developer",
+  description:
+    "I’m a soon-to-be Software Engineering graduate specializing in full-stack development with .NET and Angular. I’m passionate about building modern, maintainable applications using clean architecture, secure authentication, and efficient deployment workflows. Currently, I’m expanding my skills in .NET 8, Angular 20, and Docker, and preparing to start my Master’s degree in Software Engineering. I’m always eager to learn new technologies, explore creative ideas, and continuously grow as a developer.",
+  email: "lana-mustafic@outlook.com",
+  phone: "+387 60 335 2668",
+  address: "Sarajevo, Bosnia and Herzegovina",
+  github: "https://github.com/lana-mustafic",
+  facebook: "",
+  linkedIn: "https://linkedin.com/in/lana-mustafic",
+  twitter: "",
+  stackOverflow: "",
+  leetcode: "",
+  devUsername: "",
+  resume: "", // you can add your resume link here later
+};
