69e8f2c84db995462e419d03220c0c1a539615ff
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-11-05 14:58:46 +0100

    chore(profile): update About Me section and replace personal photo

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,9 @@
 export const personalData = {
   name: "Lana",
-  profile: "/resume_photo.jpg",
+  profile: "/github_profile_edited.jpg",
   designation: "Junior Full-Stack Developer",
   description:
-    "I’m a soon-to-be Software Engineering graduate specializing in full-stack development with .NET and Angular. I’m passionate about building modern, maintainable applications using clean architecture, secure authentication, and efficient deployment workflows. Currently, I’m expanding my skills in .NET, Angular, and Docker, and preparing to start my Master’s degree in Software Engineering. I’m always eager to learn new technologies, explore creative ideas, and continuously grow as a developer.",
+"I’m a soon-to-be Software Engineering graduate specializing in full-stack development ( .NET + Angular ) and cross-platform application development with Flutter. I build modern, maintainable applications using clean architecture, secure authentication, and efficient CI/CD workflows. I’m currently advancing my skills in .NET, Angular, Flutter, and Docker, while preparing to begin my Master’s degree in Software Engineering. Driven by curiosity and consistency, I’m committed to continuous growth and building impactful software.",
   email: "lana-mustafic@outlook.com",
   phone: "+387 60 335 2668",
   address: "Sarajevo, Bosnia and Herzegovina",
